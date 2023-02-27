2fac24c6605d0e817f841d13fd76541a176cb829
carcormir <carcormir@gmail.com>
2023-02-27 10:52:39 +0100

    basic error page without any styles

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,14 +1,46 @@
-import { Link , useLocation , useNavigate } from 'react-router-dom';
-import { useState , useCallback } from 'react';
-import { useGifs } from '../../hooks/useGifs';
-import ListOfGifs from '../../components/ListOfGifs';
-import TrendindSearches from '../../components/TrendingSearches';
-import SearchForm from 'components/SearchForm';
+import { useNavigate } from "react-router-dom";
+import SearchForm from "components/SearchForm";
+import { Helmet } from "react-helmet";
 
-export default function ErrorPage () {
-    return (
-        <>
-            <h1>404 ERROR - please help</h1>
-        </>
-    )
+const gifsErrors = [
+  "d2jjuAZzDSVLZ5kI",
+  "Bp3dFfoqpCKFyXuSzP",
+  "hv5AEBpH3ZyNoRnABG",
+  "hLwSzlKN8Fi6I",
+];
+
+export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  const randomImage = () => {
+    return `https://media.giphy.com/media/${
+      gifsErrors[Math.floor(Math.random() * gifsErrors.length) + 1]
+    }/giphy.gif`;
+  };
+
+  const handleOnClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Error 404 | Giffy</title>
+      </Helmet>
+      <header className="o-header">
+        <SearchForm />
+      </header>
+      <div className="App-wrapper">
+        <div>
+          <span>404</span>
+          <br/>
+          <span>Sometimes gettings lost isn't that bad</span>
+          <br/>
+          <img src={randomImage()} alt="alt-page-404" />
+          <br/>
+          <button onClick={handleOnClick}>Go back home</button>
+        </div>
+      </div>
+    </>
+  );
 }
