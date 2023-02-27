67bb7a2d0ebef435c67317eda1df1ad2cb429bbb
carcormir <carcormir@gmail.com>
2023-02-27 13:30:53 +0100

    css-js simple testing

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,6 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
 import { useNavigate } from "react-router-dom";
 import SearchForm from "components/SearchForm";
 import { Helmet } from "react-helmet";
+import { css, jsx } from '@emotion/react'
 
 const gifsErrors = [
   "d2jjuAZzDSVLZ5kI",
@@ -8,6 +11,38 @@ const gifsErrors = [
   "hv5AEBpH3ZyNoRnABG",
   "hLwSzlKN8Fi6I",
 ];
+
+const pageErrorStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  text-align: center;
+`
+
+const codeErrorStyles = css`
+  font-size: 5rem;
+  font-weight: bold;
+  font-style: italic;
+`
+
+const msgErrorStyles = css`
+  font-size: 1.5rem;
+  margin: 1rem auto;
+ `
+
+const SIZE = '350px'
+
+const gifErrorStyles = css({
+  margin: "1rem auto",
+  width: SIZE,
+  height: SIZE,
+  objectFit: 'cover',
+  ":hover": {
+    transform: "scale(1.1)"
+  }
+})
 
 export default function ErrorPage() {
   const navigate = useNavigate();
@@ -31,13 +66,10 @@ export default function ErrorPage() {
         <SearchForm />
       </header>
       <div className="App-wrapper">
-        <div>
-          <span>404</span>
-          <br/>
-          <span>Sometimes gettings lost isn't that bad</span>
-          <br/>
-          <img src={randomImage()} alt="alt-page-404" />
-          <br/>
+        <div css={pageErrorStyles}>
+          <span css={codeErrorStyles}>404</span>
+          <span css={msgErrorStyles}>Sometimes gettings lost isn't that bad</span>
+          <img css={gifErrorStyles} src={randomImage()} alt="alt-page-404" />
           <button onClick={handleOnClick}>Go back home</button>
         </div>
       </div>
